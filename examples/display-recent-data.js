842fce66435512d6b229f4218b497f4796c44c83
jhyder50 <100731984+jhyder50@users.noreply.github.com>
2024-09-10 07:16:01 -0400

    BRVA display-recent-data.js

--- examples/display-recent-data.js
+++ examples/display-recent-data.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const AmbientWeatherApi = require('../lib/index')
 
 const api = new AmbientWeatherApi({
-  apiKey: process.env.AMBIENT_WEATHER_API_KEY || 'Put your AW apiKey here',
-  applicationKey: process.env.AMBIENT_WEATHER_APPLICATION_KEY || 'Put your AW applicationKey here'
+  apiKey: process.env.AMBIENT_WEATHER_API_KEY || '7e1b99a6b97b4e248414daaec8df9a52262cf3cd9e2b44fc96fe0876eb67c80e',
+  applicationKey: process.env.AMBIENT_WEATHER_APPLICATION_KEY || '1a6ca5d664f04d42b93406dfdbd212b485b11712a5574d80b66e78ac8485932b'
 })
 
 // list the user's devices
