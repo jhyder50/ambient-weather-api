751901cd9287c6459f791505790e56646777607d
jhyder50 <100731984+jhyder50@users.noreply.github.com>
2024-09-10 07:10:28 -0400

    BRVArealtime.js
    connect to ambient weather api

--- examples/realtime.js
+++ examples/realtime.js
@@ -6,10 +6,10 @@ function getName (device) {
   return device.info.name
 }
 
-const apiKey = process.env.AMBIENT_WEATHER_API_KEY || 'Put your AW apiKey here'
+const apiKey = process.env.AMBIENT_WEATHER_API_KEY || '7e1b99a6b97b4e248414daaec8df9a52262cf3cd9e2b44fc96fe0876eb67c80e'
 const api = new AmbientWeatherApi({
   apiKey,
-  applicationKey: process.env.AMBIENT_WEATHER_APPLICATION_KEY || 'Put your AW applicationKey here'
+  applicationKey: process.env.AMBIENT_WEATHER_APPLICATION_KEY || '1a6ca5d664f04d42b93406dfdbd212b485b11712a5574d80b66e78ac8485932b'
 })
 
 api.connect()
